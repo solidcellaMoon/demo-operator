a549e40a6aa1b90ed49adf32bda7b82923033ad0
solidcellaMoon <solidcella@gmail.com>
2022-04-23 15:08:04 +0900

    CR 변경사항 로직 추가

--- controllers/demo_controller.go
+++ controllers/demo_controller.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors" // 추가 패키지
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -46,10 +47,38 @@ type DemoReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+
+var logger = log.Log
+
 func (r *DemoReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
 	// TODO(user): your logic here
+
+	// logger 사용법
+	logger.WithName("Resource Name")
+	logger.WithValues("Resource NameSpace: ", req.Namespace)
+	logger.Info("Resource Changed")
+
+	// CR로 정의한 객체를 가져오기 위한 struct의 ref를 받아옵니다.
+	cr := &demoappv1.Demo{}
+
+	// 데이터를 서버에서 받아와 cr에 넣어줍니다.
+	err := r.Client.Get(ctx, req.NamespacedName, cr)
+
+	// cr에 변경이 존재하거나 err가 발생한 경우 진행되는 로직입니다.
+	// 변경사항이 존재한다는 것으로 생각해야 합니다.
+	if err != nil {
+		// 변경사항인 cr이 k8s에 존재하는지 확인합니다.
+		if errors.IsNotFound(err) {
+			logger.Info("Deleted - Demo CR")
+			return ctrl.Result{}, nil
+		}
+
+		// GET 함수 처리
+		logger.Error(err, "GET CR Error occurred")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
